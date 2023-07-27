230073fe4c92980d48db31675bc01ba7ab0a2121
Yash Bhale <ybhale72@gmail.com>
2023-07-27 12:34:29 +0530

    Commit for the test coverage report

--- app/src/test/java/com/example/cicdmainapplication/ExampleUnitTest.kt
+++ app/src/test/java/com/example/cicdmainapplication/ExampleUnitTest.kt
@@ -13,10 +13,41 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+        assertEquals(4, Calculator().addition(2, 2))
+        assertEquals(10, Calculator().addition(6, 4))
+        assertEquals(22, Calculator().addition(2, 20))
     }
 
     @Test
     fun addition_isInCorrect() {
         assertNotEquals(4, 3 + 2)
+    }
+
+    @Test
+    fun subtraction_isCorrect() {
+        assertEquals(4, Calculator().subtraction(6, 2))
+        assertEquals(2, Calculator().subtraction(6, 4))
+        assertEquals(202, Calculator().subtraction(222, 20))
+    }
+
+    @Test
+    fun division_isCorrect() {
+        assertEquals(4, Calculator().division(8, 2))
+        assertEquals(3, Calculator().division(6, 2))
+        assertEquals(22, Calculator().division(220, 10))
+    }
+
+    @Test
+    fun multiplication_isCorrect() {
+        assertEquals(4, Calculator().multiply(2, 2))
+        assertEquals(30, Calculator().multiply(15, 2))
+        assertEquals(2200, Calculator().multiply(220, 10))
+    }
+
+    @Test
+    fun modulus_isCorrect() {
+        assertEquals(1, Calculator().modulus(15, 2))
+        assertEquals(2, Calculator().modulus(17, 3))
+        assertEquals(1, Calculator().modulus(10, 3))
     }
 }
